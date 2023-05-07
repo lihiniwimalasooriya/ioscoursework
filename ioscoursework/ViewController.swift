63b2c45a4f09a976aa03b6cef3c1681f2484a9ad
lihini <lswimalasooriya@gmail.com>
2023-05-07 04:22:20 -0700

    Initial Commit

---
+++ ioscoursework/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ioscoursework
+//
+//  Created by Lihini Wimalasooriya on 2023-05-07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
