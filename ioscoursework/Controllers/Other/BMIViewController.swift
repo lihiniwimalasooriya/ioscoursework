ec12651e7c6d1e7fdc6be1a47ccd7ffe26f96f61
lihini <lswimalasooriya@gmail.com>
2023-05-14 05:47:06 -0700

    BMI Calculation UI

--- ioscoursework/Controllers/Other/BMIViewController.swift
+++ ioscoursework/Controllers/Other/BMIViewController.swift
@@ -9,91 +9,132 @@ import UIKit
 
 class BMIViewController: UIViewController {
 
-    private let bmiImg: UIImageView = {
-        let iv = UIImageView()
-        iv.contentMode = .scaleAspectFit
-        iv.image = UIImage(named: "BMI" )
-        iv.tintColor = .white
-        return iv
-    } ()
-    
     private let headerView = AuthHeaderView(title: "BMI Calculation", subTitle: "Sign in to your account")
-    private let heightBMI = CustomTextField(fieldType: .heightBMI)
-    private let weightBMI = CustomTextField(fieldType: .weightBMI)
-    private let bmiButton = CustomButton(title: "Calculate", hasBackground: true, fontSize: .big)
-    private let labelHeight: UILabel = {
-            let labelHeight = UILabel()
-            labelHeight.textAlignment = .center
-            labelHeight.text = ""
-            labelHeight.font = UIFont.boldSystemFont(ofSize: 20)
-            labelHeight.textColor = .black
-            
-            return labelHeight
-        }()
-    private let labelWeight: UILabel = {
-            let labelWeight = UILabel()
-            labelWeight.textAlignment = .center
-            labelWeight.text = ""
-            labelWeight.font = UIFont.boldSystemFont(ofSize: 20)
-            labelWeight.textColor = .black
-            
-            return labelWeight
-        }()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        self.setupUserinterface()
-        self.bmiButton.addTarget(self, action: #selector(didTapBMI), for: .touchUpInside)
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        self.navigationController?.navigationBar.isHidden = true
-    }
-   
-    private func setupUserinterface() {
-        self.view.backgroundColor = .systemBackground
+        private let heightBMI = CustomTextField(fieldType: .heightBMI)
+        private let weightBMI = CustomTextField(fieldType: .weightBMI)
+        private let bmiButton = CustomButton(title: "Calculate", hasBackground: true, fontSize: .big)
+        private let labelHeight: UILabel = {
+                let labelHeight = UILabel()
+                labelHeight.textAlignment = .center
+                labelHeight.text = ""
+                labelHeight.font = UIFont.boldSystemFont(ofSize: 20)
+                labelHeight.textColor = .black
+                
+                return labelHeight
+            }()
+        private let labelWeight: UILabel = {
+                let labelWeight = UILabel()
+                labelWeight.textAlignment = .center
+                labelWeight.text = ""
+                labelWeight.font = UIFont.boldSystemFont(ofSize: 20)
+                labelWeight.textColor = .black
+                
+                return labelWeight
+            }()
         
-        self.view.addSubview(bmiImg)
-        self.view.addSubview(headerView)
-        self.view.addSubview(heightBMI)
-        self.view.addSubview(weightBMI)
-        self.view.addSubview(bmiButton)
-        self.view.addSubview(labelHeight)
-        self.view.addSubview(labelWeight)
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            self.setupUserinterface()
+            self.bmiButton.addTarget(self, action: #selector(didTapBMI), for: .touchUpInside)
+        }
         
-        bmiImg.translatesAutoresizingMaskIntoConstraints = false
-        headerView.translatesAutoresizingMaskIntoConstraints = false
-        heightBMI.translatesAutoresizingMaskIntoConstraints = false
-        weightBMI.translatesAutoresizingMaskIntoConstraints = false
-        bmiButton.translatesAutoresizingMaskIntoConstraints = false
-        labelWeight.translatesAutoresizingMaskIntoConstraints = false
-        labelHeight.translatesAutoresizingMaskIntoConstraints = false
-        
-    }
-    @objc private func didTapBMI() {
-        
-        guard let heightString = heightBMI.text,
-                         let height = Double(heightString),
-                         height > 0 else {
-                       //height.text = "Error"
-                       labelHeight.text = "Error"
-                       return
-                   }
-                   
-                   guard let weightString = weightBMI.text,
-                         let weight = Double(weightString),
-                         weight > 0 else {
-                      // label1.text = "Error"
-                       weightBMI.text = "Error"
-                       return
-                       
-                   }
-                let heightInMeters = height / 100 // Convert height from centimeters to meters
-                let bmi = weight / (heightInMeters * heightInMeters)
+        override func viewWillAppear(_ animated: Bool) {
+            super.viewWillAppear(animated)
+            self.navigationController?.navigationBar.isHidden = true
+        }
+       
+        private func setupUserinterface() {
+            self.view.backgroundColor = .systemBackground
+            let backgroundImage = UIImageView(image: UIImage(named: "BMI"))
+                 backgroundImage.contentMode = .scaleAspectFill
+                 backgroundImage.frame = view.bounds
+                 view.addSubview(backgroundImage)
+                 view.sendSubviewToBack(backgroundImage)
+            //self.view.addSubview(bmiImg)
+            self.view.addSubview(headerView)
+            self.view.addSubview(heightBMI)
+            self.view.addSubview(weightBMI)
+            self.view.addSubview(bmiButton)
+            self.view.addSubview(labelHeight)
+            self.view.addSubview(labelWeight)
+            //self.view.addSubview(bmiImg)
+          
+            //bmiImg.translatesAutoresizingMaskIntoConstraints = false
+            headerView.translatesAutoresizingMaskIntoConstraints = false
+            heightBMI.translatesAutoresizingMaskIntoConstraints = false
+            weightBMI.translatesAutoresizingMaskIntoConstraints = false
+            bmiButton.translatesAutoresizingMaskIntoConstraints = false
+            labelWeight.translatesAutoresizingMaskIntoConstraints = false
+            labelHeight.translatesAutoresizingMaskIntoConstraints = false
+          //  bmiImg.translatesAutoresizingMaskIntoConstraints = false
+            NSLayoutConstraint.activate([
+                    // bmiImg constraints
+    //                bmiImg.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+    //                bmiImg.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+    //                bmiImg.widthAnchor.constraint(equalToConstant: 100),
+    //                bmiImg.heightAnchor.constraint(equalToConstant: 100),
+    //
+                    // headerView constraints
+                   headerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+                
+                    headerView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+                    headerView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
                     
-                    // Display or use the calculated BMI value
-                labelHeight.text = "\(bmi)"
-            }
-}
-
+                    // heightBMI constraints
+                    heightBMI.topAnchor.constraint(equalTo: headerView.bottomAnchor, constant: 170),
+                    heightBMI.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+                    heightBMI.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+                    heightBMI.heightAnchor.constraint(equalToConstant: 50),
+                    
+                    // weightBMI constraints
+                    weightBMI.topAnchor.constraint(equalTo: heightBMI.bottomAnchor, constant: 20),
+                    weightBMI.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+                    weightBMI.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+                    weightBMI.heightAnchor.constraint(equalToConstant: 50),
+                    
+                    // bmiButton constraints
+                    bmiButton.topAnchor.constraint(equalTo: weightBMI.bottomAnchor, constant: 20),
+                    bmiButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+                    bmiButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+                    bmiButton.heightAnchor.constraint(equalToConstant: 50),
+                    
+                    // labelHeight constraints
+                    labelHeight.topAnchor.constraint(equalTo: bmiButton.bottomAnchor, constant: 20),
+                    labelHeight.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+                    labelHeight.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+                    
+                    // labelWeight constraints
+                    labelWeight.topAnchor.constraint(equalTo: labelHeight.bottomAnchor, constant: 20),
+                    labelWeight.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+                    labelWeight.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+                   
+                     
+                ])
+            
+            
+        }
+        @objc private func didTapBMI() {
+            
+            guard let heightString = heightBMI.text,
+                             let height = Double(heightString),
+                             height > 0 else {
+                           //height.text = "Error"
+                           labelHeight.text = "Error"
+                           return
+                       }
+                       
+                       guard let weightString = weightBMI.text,
+                             let weight = Double(weightString),
+                             weight > 0 else {
+                          // label1.text = "Error"
+                           weightBMI.text = "Error"
+                           return
+                           
+                       }
+                    let heightInMeters = height / 100 // Convert height from centimeters to meters
+                    let bmi = weight / (heightInMeters * heightInMeters)
+                        
+                        // Display or use the calculated BMI value
+                    labelHeight.text = "\(bmi)"
+                }
+    }
--- ioscoursework/Controllers/Other/CustomTextField.swift
+++ ioscoursework/Controllers/Other/CustomTextField.swift
@@ -17,47 +17,51 @@ class CustomTextField: UITextField {
             case weightBMI
         }
         
-        private let authFieldType: CustomTextFieldType
-        
-    init(fieldType: CustomTextFieldType) {
-               self.authFieldType = fieldType
-               super.init(frame: .zero)
-               
-               self.backgroundColor = .secondarySystemBackground
-               self.layer.cornerRadius = 10
-               
-               self.returnKeyType = .done
-               self.autocorrectionType = .no
-               self.autocapitalizationType = .none
-               
-               self.leftViewMode = .always
-               self.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 12, height: self.frame.size.height))
-               
-               switch fieldType {
-               case .username:
-                   self.placeholder = "Username"
-               case .email:
-                   self.placeholder = "Email Address"
-                   self.keyboardType = .emailAddress
-                   self.textContentType = .emailAddress
-               case .password:
-                   self.placeholder = "Password"
-                   self.textContentType = .oneTimeCode
-                   self.isSecureTextEntry = true
-               case .heightBMI:
-                   self.placeholder = "Height(cm)"
-                   self.textContentType = .oneTimeCode
-                   self.isSecureTextEntry = true
-               case .weightBMI:
-                   self.placeholder = "Weight(kg)"
-                   self.textContentType = .oneTimeCode
-                   self.isSecureTextEntry = true
-              
-               }
-                  }
+    private let authFieldType: CustomTextFieldType
+            
+        init(fieldType: CustomTextFieldType) {
+                   self.authFieldType = fieldType
+                   super.init(frame: .zero)
+                   
+                   self.backgroundColor = .secondarySystemBackground
+                   self.layer.cornerRadius = 10
+                   
+                   self.returnKeyType = .done
+                   self.autocorrectionType = .no
+                   self.autocapitalizationType = .none
+                   
+                   self.leftViewMode = .always
+                   self.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 12, height: self.frame.size.height))
+                   
+                   switch fieldType {
+                   case .username:
+                       self.placeholder = "Username"
+                   case .email:
+                       self.placeholder = "Email Address"
+                       self.keyboardType = .emailAddress
+                       self.textContentType = .emailAddress
+                   case .password:
+                       self.placeholder = "Password"
+                       self.textContentType = .oneTimeCode
+                       self.isSecureTextEntry = true
+                   case .heightBMI:
+                       self.placeholder = "Height(cm)"
+                       self.textContentType = .oneTimeCode
+                       self.isSecureTextEntry = true
+                       self.layer.borderWidth = 1.0
+                       self.layer.borderColor = UIColor.lightGray.cgColor
+                   case .weightBMI:
+                       self.placeholder = "Weight(kg)"
+                       self.textContentType = .oneTimeCode
+                       self.isSecureTextEntry = true
+                       self.layer.borderWidth = 1.0
+                       self.layer.borderColor = UIColor.lightGray.cgColor
                   
-                  required init?(coder: NSCoder) {
-                      fatalError("init(coder:) has not been implemented")
+                   }
+                      }
+                      
+                      required init?(coder: NSCoder) {
+                          fatalError("init(coder:) has not been implemented")
+                      }
+                      
                   }
-                  
-              }
